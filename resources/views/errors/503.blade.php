2d120b583644c29d4b4aab08d39f73c59306474b
garry.owen@wales.nhs.uk <garry.owen@users.noreply.github.com>
2025-06-09 07:46:42 +0100

    Maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<br><br><br><br><br><br>
+    <div class="card over-card">
+        <div class="card-header greenheader">
+            <strong class="text-white">{{ __('503 Error') }}</strong>
+
+            </div>
+            <div class="card-body">
+                <h1>Page not found</h1>
+                <br>
+                <p>Website undergoing essential maintenance. We'll be back soon. Sorry for any inconvenience.</p>
+            </div>  
+        </div>
+    <br><br><br>
+</div>
+@endsection
