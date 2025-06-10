9f340e81c96c85cb57245ac6024b6ed6c71c5bc1
garry.owen@wales.nhs.uk <garry.owen@users.noreply.github.com>
2025-06-10 08:02:48 +0100

    Nav fix

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -48,7 +48,7 @@
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu text-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/admin">Admin Dashboard</a>
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
