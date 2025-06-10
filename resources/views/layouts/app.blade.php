4ade370734c2fd1463782b60c8a8b145366c1449
garry.owen@wales.nhs.uk <garry.owen@users.noreply.github.com>
2025-06-10 07:51:20 +0100

    Bootstrap update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,10 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Scripts -->
-    <script src="{{asset('/js/bootstrap5.bundle.min.js') }}"></script>
+    <!-- <script src="{{asset('/js/bootstrap5.bundle.min.js') }}"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+  rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
+  crossorigin="anonymous"/>
     <script src="{{asset('/js/custom.js') }}"></script>
 
     <!-- Custom Styles -->
@@ -50,7 +53,12 @@
 
     <!-- <script src="{{asset('/js/3.5.0.jquery.min.js') }}"></script> -->
     <!-- <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-5cmQFZ4JJb+L+p+jf9zpZcZ3zvR82bLZp1f1FklZ5MjzE6LwtWZzhPrvA4d0M4PB" crossorigin="anonymous"></script>
+    <!-- Bootstrap 5.3.6 JS bundle (includes Popper) -->
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+  crossorigin="anonymous">
+</script>
 
 
 </body>
