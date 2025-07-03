6864ada06da1540080b765700f17cae40543fa99
Sirius-G <garry.owen@users.noreply.github.com>
2025-07-03 18:20:00 +0100

    JQuery fix 3.5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
             @include('inc.footer')
     </div>
 
-    <!-- <script src="{{asset('/js/3.5.0.jquery.min.js') }}"></script> -->
+    <script src="{{asset('/js/3.5.0.jquery.min.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
     </script>
     <script src="{{asset('/js/3.5.0.jquery.min.js') }}"></script>
