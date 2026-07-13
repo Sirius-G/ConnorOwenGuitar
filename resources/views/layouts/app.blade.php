d81363acb177efeb755c7d302dfdf2bc4e99a64f
Sirius-G <garry.owen@users.noreply.github.com>
2026-07-13 09:58:03 +0100

    Add missing JS link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
     </script>
     <script src="{{asset('/js/3.5.0.jquery.min.js') }}"></script>
+    <script src="{{asset('/js/custom.js') }}"></script>
 
     <!-- Custom Styles -->
     <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}" />
